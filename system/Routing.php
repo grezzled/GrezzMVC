a51b6d5e1a70f520e5c739cda0a349dc79dbbfdd
grezzled <belchhabsoufiane.official@gmail.com>
2020-05-03 02:40:11 +0000

    building the magic behind routs

--- system/Routing.php
+++ system/Routing.php
@@ -3,4 +3,8 @@
 class Routing
 {
     // TODO start building the magic behind routs
+    // (:nbr) to identify a number
+    // (:str) to identify a sequence of characters: numbers, text or symbols except for "/"
+    // eg: site.com/blog/(:str) --> site.com/blog/getArticle/$1 and $1 is the blog webId
+    // eg: site.com/page/(:nbr) --> site.com/page/getPage/$1 and $1 is the page ID
 }
