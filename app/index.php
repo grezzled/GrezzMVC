1d957e41fc0f35e821ed613a733dce77316444ae
grezzled <belchhabsoufiane.official@gmail.com>
2020-05-12 21:19:49 +0000

    replace htaccess with index

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright (c) 2020.
+ * Created By grezzled
+ * @package    GrezzMVC
+ * @author     grezzled
+ * @copyright  2020.
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://isoufiane.com
+ * @since      Version 1.0.3
+ */
+
+// Silence is the best reply to a fool.
